b2d42b15a35ef690e90ef446ca10d0efde9c543b
tristky <tristky@gmail.com>
2023-10-25 17:37:52 +0800

    Added new primevue features for marketplace

--- Xanadu/src/main.js
+++ Xanadu/src/main.js
@@ -3,5 +3,8 @@ import App from "./App.vue";
 import router from "./router/index.js";
 import VueChartkick from "vue-chartkick";
 import "chartkick/chart.js";
+import 'primevue/resources/themes/saga-blue/theme.css';     // theme
+import 'primevue/resources/primevue.min.css';              // core css
+import 'primeicons/primeicons.css';                        // icons
 
 createApp(App).use(router).use(VueChartkick).mount("#app");
