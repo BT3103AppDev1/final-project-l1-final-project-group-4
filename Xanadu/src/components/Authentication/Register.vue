7df8d7a942bfb51452af4c4b190ea4cd8025d421
brian-h123 <134823170+brian-h123@users.noreply.github.com>
2023-11-02 03:01:46 +0800

    finish styling for register/login page
    Finished styling for register / login page
    - made userType input required (will give alert if none is selected)
    - generate alert messages if register/login failed (invalid password, invalid email,....)

--- Xanadu/src/components/Authentication/Register.vue
+++ Xanadu/src/components/Authentication/Register.vue
@@ -1,98 +1,96 @@
 <template>
     <div class="container">
         <form @submit.prevent="register" id="registerForm">
-            <h2>Register</h2>
-            <div class="input">
-                Select user type:
+            <h1 class="CreateAccountTitle">Create Account</h1>
 
-                <div class="flex flex-wrap gap-3">
-                    <div class="flex align-items-center" style="display: inline-block; margin-right: 10px">
-                        <RadioButton 
-                            v-model="userType"
-                            inputId="userTypeEcoEntrpreneur"
-                            name="userTypeSelection"
-                            value="Eco-Entrepreneur"
+            <div class="flex justify-content-center p-fluid">
+                <div class="field">
+                    <span class="p-float-label">
+                        <InputText
+                            id="inputFirstName"
+                            v-model="firstName"
+                            type="text"
                             required
                         />
-                        <label for="userTypeEcoEntrepreneur" class="ml-2">Eco-Entrepreneur</label>
+                        <label for="inputFirstName">First Name</label>
+                    </span>
+                </div>
+
+                <div class="field">
+                    <span class="p-float-label">
+                        <InputText
+                            id="inputLastName"
+                            v-model="lastName"
+                            type="text"
+                            required
+                        />
+                        <label for="inputLastName">Last Name</label>
+                    </span>
+                </div>
+
+                <div class="field">
+                    <span class="p-input-icon-right p-float-label">
+                        <i class="pi pi-envelope" />
+                        <InputText
+                            id="email"
+                            v-model="email"
+                            type="email"
+                            required
+                        />
+                        <label for="email">Email</label>
+                    </span>
+                </div>
+
+                <div class="field">
+                    <div class="p-float-label">
+                        <Password v-model="password" id="password" required>
+                            <template #header>
+                                <h3>Pick a password</h3>
+                            </template>
+
+                            <template #footer>
+                                <Divider />
+                                <p class="mt-2">Suggestions</p>
+                                <ul>
+                                    <li>At least one lowercase</li>
+                                    <li>At least one uppercase</li>
+                                    <li>At least one numeric</li>
+                                    <li>Minimum 8 characters</li>
+                                </ul>
+                            </template>
+                        </Password>
+                        <label for="password">Password</label>
                     </div>
-                    
-                    <div class="flex align-items-center" style="display: inline-block">
-                        <RadioButton
+                </div>
+
+                <div class="field userTypeField">
+                    <RadioButton 
+                        v-model="userType"
+                        inputId="userTypeEcoEntrpreneur"
+                        name="userTypeSelection"
+                        value="Eco-Entrepreneur"
+
+                        class="userTypeInput_style"
+                    />
+                    <label for="userTypeEcoEntrepreneur" class="radiobutton-label userTypeInput_style">Eco-Entrepreneur</label>
+
+                    <RadioButton
                         v-model="userType"
                         inputId="userTypeGreenRanger"
                         name="userTypeSelection"
                         value="Green Ranger"
-                        required
-                        />
-                        <label for="userTypeGreenRanger" class="ml-2">Green Ranger</label>
-                    </div>
 
-                    <div class="flex justify-content-center p-fluid">
-                        <div v-focustrap class="card">
-                            <div class="field">
-                                <InputText
-                                id="inputFirstName"
-                                v-model="firstName"
-                                type="text"
-                                placeholder="First Name"
-                                autofocus
-                                />
-                            </div>
-
-                            <div class="field">
-                                <InputText
-                                id="inputLastName"
-                                v-model="lastName"
-                                type="text"
-                                placeholder="Last Name"
-                                autofocus
-                                />
-                            </div>
-
-                            <div class="field">
-                                <div class="p-input-icon-right">
-                                    <i class="pi pi-envelope" />
-                                    <InputText
-                                        id="email"
-                                        v-model="email"
-                                        type="email"
-                                        placeholder="Email"
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="field">
-                                <div class="p-float-label">
-                                    <Password v-model="password">
-                                        <template #header>
-                                        <h6>Pick a password</h6>
-                                        </template>
-                                        <template #footer>
-                                        <Divider />
-                                        <p class="mt-2">Suggestions</p>
-                                        <ul class="pl-2 ml-2 mt-0" style="line-height: 1.5">
-                                            <li>At least one lowercase</li>
-                                            <li>At least one uppercase</li>
-                                            <li>At least one numeric</li>
-                                            <li>Minimum 8 characters</li>
-                                        </ul>
-                                        </template>
-                                    </Password>
-                                    
-                                    <label for="password">Password</label>
-                                </div>
-                                <br />
-                            </div>
-                            <Button type="register" label="Register" class="mt-2" />
-                        </div>
-                    </div>
+                        class="userTypeInput_style"
+                    />
+                    <label for="userTypeGreenRanger" class="radiobutton-label userTypeInput_style">Green Ranger</label>
                 </div>
-                
-            </div>
 
-            <div class="alternative option">
-                Already have an account? <span @click="moveToLogin">Login</span>
+                <Button type="register" label="Register" class="mt-2 register-button" />
+            </div>                
+
+            <div class="alternative_option">
+                Already have an account? 
+                <span @click="moveToLogin" class="switchTo_login">Login</span>
             </div>
 
         </form>
@@ -118,6 +116,11 @@ export default {
 
     methods: {
         register() {
+            if (this.userType==""){
+                alert("Please select an Account Type you would like to register as!")
+                return
+            }
+
             const auth = getAuth();
             const db = getFirestore();
 
@@ -129,7 +132,7 @@ export default {
                     lastName: this.lastName,
                     email: this.email,
                     });
-                } else {
+                } else if (this.userType == "Green Ranger"){
                     setDoc(doc(db, "Green Rangers", userCredential.user.uid), {
                     firstName: this.firstName,
                     lastName: this.lastName,
@@ -151,14 +154,25 @@ export default {
                 this.$router.push("/dashboard");
                 })
                 .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode);
-                console.log(errorMessage);
-                let alert_2 = document.querySelector("#alert_2");
-                alert_2.classList.remove("d-none");
-                alert_2.innerHTML = errorMessage;
-                console.log(alert_2);
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log(errorCode);
+                    console.log(errorMessage);
+
+                    if (errorCode == "auth/invalid-email") {
+                        alert("Invalid Email!\nPlease key in a valid email address!")
+                    } else if (errorCode == "auth/missing-password"){
+                        alert("Missing Password! \nYou need to key in a valid password to register an account")
+                    } else if (errorCode == "auth/weak-password") {
+                        alert("Weak Password! \nPlease key in a strong password! \nA valid password should be at least 6 characters.")
+                    } else if (errorCode == "auth/email-already-in-use") {
+                        alert("Email exists! \nEmail already in use!\n Please use another email or login instead!")
+                    } 
+
+                    // let alert_2 = document.querySelector("#alert_2");
+                    // alert_2.classList.remove("d-none");
+                    // alert_2.innerHTML = errorMessage;
+                    // console.log(alert_2);
                 });
         },
 
@@ -170,11 +184,90 @@ export default {
 </script>
 
 <style scoped>
-.container{
+/* import specific font */
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+
+.CreateAccountTitle{
+    font-size: 32px; 
+    font-family: 'Montserrat';
+    line-height: 50px; 
     text-align: center;
 }
 
-.input{
-    margin: 10px;
+.userTypeField{
+    padding-left: 10px;
+    height: fit-content;
 }
-</style>
+
+.userTypeInput_style {
+    margin-right: 5px;
+}
+
+.radiobutton-label{
+    margin-right: 15px;
+}
+
+/* use :deep to override the primevue css */
+:deep(.p-radiobutton-box){
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    border: 1px solid #999;
+    border-radius: 15%;
+}
+
+:deep(.p-radiobutton-icon){
+    background: none;
+}
+
+:deep(.p-radiobutton-icon::before){
+    appearance: none;
+    display: flex;
+    height: 100%;
+    content: "\2713";
+    flex-wrap: wrap;
+    align-content: center;
+    justify-content: center;
+    color: white;
+}
+
+.register-button{
+    background-color: #5A6D57;
+    border: none;
+}
+
+.field{
+    padding-bottom: 20px;
+}
+
+li{
+    display: list-item;
+    margin-left: 1rem;
+    list-style-position: inside;
+    list-style: circle;
+}
+
+.container{
+    width: 50%;
+    margin: auto;
+}
+
+.alternative_option{
+    margin-top: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.switchTo_login{
+    margin-left: 20px;
+    padding: 5px;
+    color: #748C70;
+    
+}
+
+.switchTo_login:hover{
+    background-color: rgba(226, 226, 226, 0.79);
+    outline: rgb(201, 201, 201) 0.5px solid;
+}
+</style>    
