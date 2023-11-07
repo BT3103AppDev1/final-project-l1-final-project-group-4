ec38deab1a097d8f4bdac6a42bf21ad135c1c335
melissa-han <99945078+melissa-han@users.noreply.github.com>
2023-11-07 17:13:56 +0800

    seller order table added

--- Xanadu/src/views/Orders.vue
+++ Xanadu/src/views/Orders.vue
@@ -19,7 +19,24 @@ export default {
             user: null,
             userType: null,
             orders: [],
-            count: 0
+            count: 0,
+            selectedProduct: []
+        }
+    },
+
+    watch: {
+        selectedProduct(newlyfulfilledproducts, oldfulfilledproducts) {
+            oldfulfilledproducts.filter(product =>{
+                !newlyfulfilledproducts.some(product2 => {
+                    product.id == product2.id;
+                })
+            })
+            for (let product of oldfulfilledproducts) {
+                product.status = "pending";
+            }
+            for (let product of newlyfulfilledproducts) {
+                product.status = "fulfilled";
+            }
         }
     },
 
@@ -31,6 +48,8 @@ export default {
             const userDocRef = doc(db, 'Users', user.uid);
             const userDoc = await getDoc(userDocRef);
             this.userType = userDoc.data().userType;
+
+            if (this.userType == "Green Ranger") {
             
             const orders = [];
             const ordersRef = collection(db, 'Green Rangers', user.uid, 'Orders');
@@ -75,13 +94,41 @@ export default {
             });
             }
             this.orders = orders.sort((a, b) => b.orderPlacedAt - a.orderPlacedAt); // Assuming you want newest first
-            }
-        });
-        },
-}
+            } else {
+                const orders = [];
+                const ordersRef = collection(db, 'Eco-Entrepreneur', user.uid, 'Orders');
+                const allOrdersSnapshot = await getDocs(ordersRef);
+                for (const orderDoc of allOrdersSnapshot.docs) {
+
+
+                    const productRef = doc(db, "Products", orderDoc.data().productName);
+                    const productSnapshot = await getDoc(productRef);
+                    console.log(productSnapshot.data().pictures);
+                    let pictures = [];
+                    if (productSnapshot.exists()) {
+                        pictures = productSnapshot.data().pictures || [];
+                    }
+                    orders.push({
+                        order: orderDoc.id,
+                        address: orderDoc.data().shippingAddress,
+                        product: orderDoc.data().productName,
+                        orderPlacedAt: orderDoc.data().orderPlacedAt.toDate(),
+                        orderDate: orderDoc.data().orderPlacedAt.toDate().toDateString(),
+                        productPicture: pictures,
+                        buyer: orderDoc.data().buyerUsername,
+                        quantity: orderDoc.data().productQuantity,
+                        status: "pending"
+                    })    
+                };
+                this.orders = orders.sort((a, b) => b.orderPlacedAt - a.orderPlacedAt);
+                console.log(this.orders);
+            }   
+    }})
+}}
 </script>
 
 <template>
+<div v-if = "userType == 'Green Ranger'" > 
     <div>
         <Divider align="center" :class="$style.ordersHeader">PAST ORDERS:</Divider>
     </div>
@@ -133,6 +180,28 @@ export default {
         </Card>
     </div>
     </div>
+</div>
+<div v-else>
+    <div>
+        <Divider align="center" :class="$style.ordersHeader">ORDERS:</Divider>
+    </div>
+    <div>
+        <DataTable v-model:selection="selectedProduct"  sortField="status" :sortOrder="-1" :value = "orders" tableStyle="min-width: 50rem">
+            <Column field="order" header="Order ID"></Column>
+                <Column field="product" header="Product"></Column>
+                <Column header="Product Image">
+                    <template #body="slotProps">
+                        <Image :src="slotProps.data.productPicture" alt="Image" width="100" preview />
+                    </template>
+                </Column>
+                <Column field="quantity" header="Quantity"></Column>
+                <Column field="address" header="Address"></Column>
+                <Column field="orderDate" header="Order Date" sortable></Column>
+                <Column field="status" header="Order Status"></Column>
+                <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+        </DataTable>
+    </div>
+</div>
     
 </template>
 
