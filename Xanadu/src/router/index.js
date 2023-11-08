6a5323d3851480cad0371330cf428271bbed33da
Ke Xin <lmhforever17@gmail.com>
2023-11-09 00:04:21 +0800

    Revert "Merge pull request #16 from BT3103AppDev1/kexin"
    This reverts commit c035f29ad9f6958d8b8c551d545946d609554e41, reversing
    changes made to 2d4090a2719651aeea0d20c45647ff2b9dd67883.

--- Xanadu/src/router/index.js
+++ Xanadu/src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 // import DashboardGR from "../views/DashboardViewGR.vue";
 // import DashboardEE from "../views/DashboardViewEE.vue";
 import Dashboard from "../views/DashboardView.vue";
-import Landing from "../views/LandingView.vue";
 import Forum from "../views/ForumView.vue";
 import Marketplace from "../views/MarketplaceView.vue";
 import Confirmation from "@/views/Confirmation.vue";
@@ -21,12 +20,6 @@ import Profile from "../views/ProfileView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/",
-      name: "landing",
-      component: Landing,
-    },
-
     {
       path: "/dashboard",
       name: "dashboard",
