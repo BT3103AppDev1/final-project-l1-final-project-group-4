96e3b9885ebb8089a9989b9d7383184f69a764d5
Ke Xin <lmhforever17@gmail.com>
2023-11-08 21:58:27 +0800

    landing

--- Xanadu/src/views/LandingView.vue
+++ Xanadu/src/views/LandingView.vue
@@ -1,17 +1,44 @@
 <template>
     <div class="background-container">
-      <!-- Your content here -->
+      <h1 class="welcome-text">Welcome To</h1>
+      <div class="logo-container">
+        <img alt="logo" src="../assets/landing.png">
+      </div>
     </div>
   </template>
   
   <style scoped>
   .background-container {
     background-image: url('../assets/background.png');
-    background-size: cover; /* This will cover the entire container area */
-    background-position: center; /* Center the image */
-    background-repeat: no-repeat; /* Do not repeat the image */
-    height: 87vh; /* Full viewport height */
-    width: 100vw; /* Full viewport width */
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 90vh;
+    width: 100vw;
+  }
+  
+  .logo-container img {
+    max-width: 80%;
+    height: auto;
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 52%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+  }
+
+  
+  .welcome-text {
+    position: absolute;
+    top: 30%; /* Position the "Welcome" text above the logo */
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    font-size: 2rem;
+    color: #666666; /* Adjust the color as needed */
+    font-weight: 660;
   }
   </style>
+  
   
